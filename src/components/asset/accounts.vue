3188008e158c61347eb0934d6cb8bdd00a9154ef
SONAMJU <skawn1228@gmail.com>
2024-11-12 16:12:29 +0900

    refactor: 컴포넌트 통합

---
+++ src/components/asset/accounts.vue
@@ -0,0 +1,39 @@
+<script setup>
+import FreeAccount from './account.vue'
+
+defineProps({
+  title: String,
+  accounts: Object,
+})
+</script>
+
+<template>
+  <div class="free-accounts-container">
+    <h2>{{ title }}</h2>
+
+    <div class="account-list">
+      <FreeAccount v-for="account in accounts" :key="account.accountId" :account="account" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.free-accounts-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.free-accounts-container > h2 {
+  font-weight: 600;
+}
+
+.account-list {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
