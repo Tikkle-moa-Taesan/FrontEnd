e9a106f25414ad5a373ad7cdbd08e1d495307f3f
SONAMJU <skawn1228@gmail.com>
2024-11-13 13:06:58 +0900

    feat: 계좌 상세정보 컴포넌트 생성

---
+++ src/components/asset/AccountInfo.vue
@@ -0,0 +1,95 @@
+<script setup>
+import formatNumber from '@/utils/formatNumber'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const accountType = ref(route.params.type)
+const accountId = ref(route.params.id)
+
+const accountInfo = ref({
+  accountId: 1,
+  balance: 7730000,
+  bankName: '하나은행',
+  difference: 50000,
+  maturityDate: '2025-11-12',
+  interestRate: 4.3,
+  accountName: '뱅크월렛 카카오통장',
+})
+</script>
+
+<template>
+  <div class="account-info-container">
+    <div class="account-main-info">
+      <span class="account-name">{{ accountInfo.accountName }}</span>
+      <span class="account-balance">{{ formatNumber(accountInfo.balance) }}원</span>
+    </div>
+
+    <span class="account-detail">
+      {{ accountInfo.bankName }}
+    </span>
+
+    <div v-if="accountType === 'saving'" class="saving-account-detail">
+      <div class="content">
+        <span class="label">적용 금리</span>
+        <span class="value">{{ accountInfo.interestRate }}%</span>
+      </div>
+      <div class="content">
+        <span class="label">만기일</span>
+        <span class="value">{{ accountInfo.maturityDate }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.account-info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+
+.account-main-info {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+
+.account-name {
+  font-weight: 700;
+  color: #646464;
+}
+
+.account-balance {
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+
+.account-detail {
+  font-size: 14px;
+  color: #646464;
+}
+
+.saving-account-detail {
+  display: flex;
+  gap: 3.75rem;
+  padding: 0.5rem;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+}
+
+.saving-account-detail .content {
+  display: flex;
+  gap: 0.5rem;
+  font-size: 0.875rem;
+}
+
+.saving-account-detail .label {
+  color: #646464;
+}
+</style>
