93655e0556230ce76787c97ab72d4cedb183d430
SONAMJU <skawn1228@gmail.com>
2024-11-14 10:28:06 +0900

    feat: 월별 수입지출 요약 컴포넌트 생성

---
+++ src/components/financialLedger/MonthlySummary.vue
@@ -0,0 +1,86 @@
+<script setup>
+import formatNumber from '@/utils/formatNumber'
+import { ref } from 'vue'
+
+const month = ref(new Date().getMonth() + 1)
+
+const data = ref({
+  income: 3000000,
+  expense: 2500000,
+})
+</script>
+
+<template>
+  <div class="summary-container">
+    <h2 class="title">
+      <span> {{ month }}월 내역 </span>
+      <img class="arrow-img" src="@/assets/icons/arrow-down.png" alt="화살표" />
+    </h2>
+
+    <div class="income-and-expense">
+      <div class="value-container">
+        <span>지출</span>
+        <span>
+          <span class="expense-value">
+            {{ formatNumber(data.expense) }}
+          </span>
+          원
+        </span>
+      </div>
+      <div class="value-container">
+        <span>수입</span>
+        <span>
+          <span class="income-value">
+            {{ formatNumber(data.income) }}
+          </span>
+          원
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.summary-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+
+.summary-container .title {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+
+.summary-container .arrow-img {
+  width: 1rem;
+  height: 0.7rem;
+}
+
+.income-and-expense {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  font-weight: 600;
+}
+
+.value-container {
+  display: flex;
+  gap: 1rem;
+}
+
+.value-container .expense-value {
+  color: #ff3939;
+}
+
+.value-container .income-value {
+  color: #3396f4;
+}
+</style>
