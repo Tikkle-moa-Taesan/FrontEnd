fedc7abb7e6bffe35fb70ce38d771b6785ba1845
SONAMJU <skawn1228@gmail.com>
2024-11-15 15:34:47 +0900

    refactor: 자산 상세 페이지 컴포넌트 구조 변경

---
+++ src/components/asset/AccountTransaction.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  accountTransaction: Array,
+})
+</script>
+
+<template>
+  <div class="account-transaction-container"></div>
+</template>
+
+<style scoped>
+.account-transaction-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 10px;
+}
+</style>
