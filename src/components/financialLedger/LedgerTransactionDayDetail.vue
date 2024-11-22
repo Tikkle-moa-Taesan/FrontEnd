f7ec7e2fca74d27fa32138282382a624edeaf223
SONAMJU <skawn1228@gmail.com>
2024-11-22 15:18:23 +0900

    feat: 카테고리 아이콘 추가

--- src/components/financialLedger/LedgerTransactionDayDetail.vue
+++ src/components/financialLedger/LedgerTransactionDayDetail.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CATEGORIES from '@/constants/category'
 import formatNumber from '@/utils/formatNumber'
 import { computed } from 'vue'
 
@@ -9,13 +10,24 @@ const props = defineProps({
 const type = computed(() => {
   return props.transaction.transactionType === 'expense' ? '-' : '+'
 })
+
+const findCategoryIcon = computed(() => {
+  const categoryArr = CATEGORIES.find((elem) => elem.kor === props.transaction.categoryName)
+
+  return categoryArr.src
+})
 </script>
 
 <template>
   <div class="detail-container">
-    <div class="description">
-      <span>{{ transaction.merchantName }}</span>
-      <span class="text-gray">{{ transaction.categoryName }}</span>
+    <div class="description-container">
+      <div class="img-container">
+        <img :src="findCategoryIcon" alt="" />
+      </div>
+      <div class="description">
+        <span>{{ transaction.merchantName }}</span>
+        <span class="text-gray">{{ transaction.categoryName }}</span>
+      </div>
     </div>
     <div class="money">
       <span class="spending-money" :class="{ 'text-blue': type === '+' }"
@@ -30,6 +42,27 @@ const type = computed(() => {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.description-container {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+}
+
+.img-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2.75rem;
+  height: 2.75rem;
+  background-color: #d9d9d9;
+  border-radius: 999px;
+  overflow: hidden;
+}
+
+.img-container img {
+  width: 60%;
 }
 
 .description {
