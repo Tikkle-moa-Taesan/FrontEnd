c99aa68fe3a3a54deed25e31bca4874774d01c3a
SONAMJU <skawn1228@gmail.com>
2024-11-23 21:49:30 +0900

    style: 메인 페이지 로딩 처리 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,9 +13,6 @@ import {
   getTotalBalance,
   postBudgetUpdate,
 } from '@/utils/api'
-import Loading from '@/components/commons/Loading.vue'
-
-const MINIMUM_LOADING_TIME = 500
 
 const isLoading = ref(true)
 
@@ -28,8 +25,6 @@ const monthlySpending = ref({})
 const monthlyBudget = ref({})
 
 onMounted(async () => {
-  const startTime = Date.now()
-
   await postBudgetUpdate()
 
   profile.value = await getProfile()
@@ -42,36 +37,16 @@ onMounted(async () => {
   monthlySpending.value = await getExpenseStatistic()
   monthlyBudget.value = await getBudgetStatistic()
 
-  const elapsedTime = Date.now() - startTime
-
-  if (elapsedTime < MINIMUM_LOADING_TIME) {
-    setTimeout(() => {
-      isLoading.value = false
-    }, MINIMUM_LOADING_TIME - elapsedTime)
-  } else {
-    isLoading.value = false
-  }
+  isLoading.value = false
 })
 </script>
 
 <template>
-  <div v-if="!isLoading" class="page-container">
-    <ToTalBalance :profile="profile" :total-balance="totalBalance" />
-    <MajorAccounts :free-account-list="freeAccountList" />
-    <SpendingStatistic :monthly-spending="monthlySpending" :monthly-budget="monthlyBudget" />
-  </div>
-
-  <div v-else class="loading-wrapper">
-    <Loading msg="데이터를 불러오고 있어요." />
+  <div class="flex-one">
+    <div v-if="!isLoading" class="page-container">
+      <ToTalBalance :profile="profile" :total-balance="totalBalance" />
+      <MajorAccounts :free-account-list="freeAccountList" />
+      <SpendingStatistic :monthly-spending="monthlySpending" :monthly-budget="monthlyBudget" />
+    </div>
   </div>
 </template>
-
-<style scoped>
-.loading-wrapper {
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #f2f2f2;
-}
-</style>
