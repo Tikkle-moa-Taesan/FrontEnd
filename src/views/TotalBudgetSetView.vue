3971cf0fefbb043103ab0a70154b8040540da3cd
SONAMJU <skawn1228@gmail.com>
2024-11-24 20:44:08 +0900

    feat: 총 예산 설정 시, 초기 값으로 이전 데이터 삽입

--- src/views/TotalBudgetSetView.vue
+++ src/views/TotalBudgetSetView.vue
@@ -1,13 +1,34 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+
 import LastMonthSummary from '@/components/financialLedger/LastMonthSummary.vue'
 import TotalBudgetForm from '@/components/financialLedger/TotalBudgetForm.vue'
+import { getBudgetStatistic } from '@/utils/api'
+
+const isLoading = ref(true)
+
+const previousBudget = ref(0)
+
+onMounted(async () => {
+  const res = await getBudgetStatistic()
+  previousBudget.value = res.thisMonthBudget
+
+  isLoading.value = false
+})
 </script>
 
 <template>
-  <div class="page-container">
-    <LastMonthSummary />
-    <TotalBudgetForm />
+  <div class="flex-one">
+    <div v-if="!isLoading" class="page-container">
+      <LastMonthSummary />
+      <TotalBudgetForm :previous-budget="previousBudget" />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.flex-one {
+  display: flex;
+  flex-direction: column;
+}
+</style>
